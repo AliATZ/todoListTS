4b23493f12a1bb98cc21b793e4dbd5976d7ab4ca
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-06-24 23:47:25 +0330

    Add Save to Local Storage and Delete Button

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ const form = document.querySelector<HTMLFormElement>('#new-todo-form')!
 const todoInput = document.querySelector<HTMLInputElement>('#todo-input')!
 const list = document.querySelector<HTMLUListElement>('#list')!
 
-const todos = [];
+let todos: Todo[] = loadTodos();
+todos.forEach(renderNewTodo)
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -26,6 +27,7 @@ form.addEventListener('submit', e => {
     }
     todos.push(newTodo)
     renderNewTodo(newTodo)
+    saveTodos()
     todoInput.value =""
 })
 
@@ -41,7 +43,7 @@ function renderNewTodo (todo:Todo){
     checkbox.classList.add("label-input")
     checkbox.addEventListener("change", () => {
         todo.completed = checkbox.checked
-
+        saveTodos()
     })
 
     const textEl = document.createElement('span')
@@ -51,8 +53,25 @@ function renderNewTodo (todo:Todo){
     const deleteBtn = document.createElement('button')
     deleteBtn.classList.add('delete-btn')
     deleteBtn.innerHTML = "delete"
+    deleteBtn.addEventListener("click", () => {
+        listItem.remove()
+        todos = todos.filter(t => t.id !== todo.id )
+        saveTodos()
+    })
 
     label.append(checkbox,textEl)
     listItem.append(label,deleteBtn)
     list.append(listItem)
+}
+
+function saveTodos() {
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+function loadTodos(){
+    const value = localStorage.getItem('todos')
+    if (value == null) {
+        return []
+    }
+    return JSON.parse(value) as Todo[]
 }
